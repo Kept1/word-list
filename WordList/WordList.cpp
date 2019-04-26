8d4d69a1e776a6e7fe727be55ea76172ba192617
Kept1 <Konnik.oleg.05@mail.ru>
2019-04-26 11:30:08 +0300

    Fix the error.

--- WordList/WordList.cpp
+++ WordList/WordList.cpp
@@ -17,11 +17,11 @@ int main()
 	bool b = false;
 	string w;
 	int schet;
-	cout << "Vvod slova " << endl;
+	cout << "Input of the word:" << endl;
 	cin >> w;
 	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0; j < spisok[i]; j++) {
+		for (int j = 0; j < spisok[i].length(); j++) {
 			if (w[j] == spisok[i][j]) {
 				if (schet == n) {
 					b == true;
