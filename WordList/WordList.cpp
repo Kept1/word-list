3957700570146bde56b581886129e449e5249d18
Kept1 <Konnik.oleg.05@mail.ru>
2019-04-26 10:53:42 +0300

    Starting of project

---
+++ WordList/WordList.cpp
@@ -0,0 +1,39 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<string> spisok(n);
+	for (int i = 0; i < n; i++) {
+		cout << "Word number " << i + 1 << " - this is " << endl;
+		cin >> spisok[i];
+	}
+	bool b = false;
+	string w;
+	int schet;
+	cout << "Vvod slova " << endl;
+	cin >> w;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < spisok[i]; j++) {
+			if (w[j] == spisok[i][j]) {
+				if (schet == n) {
+					b == true;
+				}
+				schet++;
+			}
+		}
+	}
+	if (b == true) {
+		cout << "Yes" << endl;
+	}
+	else {
+		cout << "No" << endl;
+	}
+}
